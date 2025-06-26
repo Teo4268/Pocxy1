ffa0ead121af285cfd818d070e630e1116c4c9c9
Teo4268 <mamacook3300@gmail.com>
2025-06-26 19:15:02 +0700

    Add files via upload

--- src/modules/allow/index.js
+++ src/modules/allow/index.js
@@ -1,20 +1,31 @@
-// Danh sách IP:HOST được phép
+// Logs
+var mes = require('../../message');
+
+
+// Allowed IP:HOST to proxy to.
 var allowed_ip = require('../../../allowed');
 
+
+// This method will check if this websocket can proxy to this server
+// next(boolean) will expect a true or false
+//
+// @param {Object}
+// @param {Function} next module to execute from stack
 function checkAllowed(info, next) {
 	var target = info.req.url.substr(1);
 	var from   = info.req.connection.remoteAddress;
 
-	// Reject nếu không nằm trong danh sách
+	// Reject
 	if (allowed_ip.length && allowed_ip.indexOf(target) < 0) {
-		console.log("Reject requested connection from '%s' to '%s'.", from, target);
+		mes.info("Reject requested connection from '%s' to '%s'.", from, target);
 		next(false);
-		return;
 	}
 
 	next(true);
 }
 
+
+// Exports methods
 module.exports = {
-	verify: checkAllowed
+	verify: checkAllowed //module.verify method
 }
