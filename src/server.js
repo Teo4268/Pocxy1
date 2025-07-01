b221a99274c0f6758a3626f1b6c30efc8a61167a
Teo4268 <mamacook3300@gmail.com>
2025-07-01 10:58:58 +0700

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -1,69 +1,86 @@
 /**
  * Dependencies
  */
-const http    = require('http');
-const https   = require('https');
-const fs      = require('fs');
-const ws      = require('ws');
-const modules = require('./modules');
-const mes     = require('./message');
-const Proxy   = require('./proxy'); // Proxy constructor
+var http    = require('http');
+var https   = require('https');
+var fs      = require('fs');
+var ws      = require('ws');
+var modules = require('./modules');
+var mes     = require('./message');
+
+
+/**
+ * Proxy constructor
+ */
+var Proxy = require('./proxy');
+
 
 /**
  * Initiate a server
  */
-const Server = function Init(config) {
-	const opts = {
+var Server = function Init(config) {
+	var opts = {
 		clientTracking: false,
 		verifyClient:   onRequestConnect
-	};
+	}
 
-	if (config.ssl) {
+	if(config.ssl) {
 		opts.server = https.createServer({
-			key: fs.readFileSync(config.key),
-			cert: fs.readFileSync(config.cert),
-		}, (req, res) => {
+			key: fs.readFileSync( config.key ),
+			cert: fs.readFileSync( config.cert ),
+		}, function(req, res) {
 			res.writeHead(200);
-			res.end("Secure wsProxy running...\n");
+        	res.end("Secure wsProxy running...\n");
 		});
 
-		opts.server.listen(config.port);
-		mes.status("Starting a secure wsProxy on port %s...", config.port);
-	} else {
-		opts.server = http.createServer((req, res) => {
+		opts.server.listen(config.port)
+
+		mes.status("Starting a secure wsProxy on port %s...", config.port)
+	}
+	else {
+		opts.server = http.createServer(function(req, res) {
 			res.writeHead(200);
 			res.end("wsProxy running...\n");
 		});
 
-		opts.server.listen(config.port);
-		mes.status("Starting wsProxy on port %s...", config.port);
+		opts.server.listen(config.port)
+
+		mes.status("Starting wsProxy on port %s...", config.port)
 	}
 
-	const WebSocketServer = new ws.Server(opts);
+	var WebSocketServer = new ws.Server(opts)
 
-	// ✅ Phải khai báo (ws, req) để lấy req.url
 	WebSocketServer.on('connection', onConnection);
 
 	return this;
-};
+}
+
 
 /**
- * Xác minh trước khi cho kết nối vào
+ * Before estabilishing a connection
  */
 function onRequestConnect(info, callback) {
-	modules.method.verify(info, (res) => {
+
+	// Once we get a response from our modules, pass it through
+	modules.method.verify(info, function(res) {
 		callback(res);
-	});
+	})
+
 }
 
+
 /**
- * Khi đã xác minh xong, tạo kết nối Proxy
+ * Connection passed through verify, lets initiate a proxy
  */
-function onConnection(ws, req) {
-	modules.method.connect(ws, (res) => {
-		new Proxy(ws, req); // ✅ Truyền thêm `req`
-	});
+function onConnection(ws) {
+
+	modules.method.connect(ws, function(res) {
+		//All modules have processed the connection, lets start the proxy
+		new Proxy(ws);
+	})
+
 }
+
 
 /**
  * Exports
